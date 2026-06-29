caaec572de83e2681f97f118f2a02d71834538df
alexcm06 <camposmarinalexander6@gmail.com>
2026-06-29 14:50:28 -0600

    Commit inicial TransLogi

---
+++ src/main/java/com/TransLogi/TransLogiApplication.java
@@ -0,0 +1,13 @@
+package com.TransLogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransLogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransLogiApplication.class, args);
+	}
+
+}
